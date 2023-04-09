3f33349223247f7af4a2a197a0f5727641a7b726
12356544567875 <109077342+12356544567875@users.noreply.github.com>
2023-04-10 00:18:21 +0530

    Create var.h

---
+++ var.h
@@ -0,0 +1,11 @@
+// this file contains all the variables for app
+
+#include <iostream>
+
+std::string colom_1;
+std::string colom_2;
+std::string colom_3;
+std::string colom_4;
+std::string colom_5;
+std::string colom_6;
+std::string colom_7;
