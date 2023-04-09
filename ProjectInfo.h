0be8226f2af92e16afc3cfe2e33813adb75b5a82
12356544567875 <109077342+12356544567875@users.noreply.github.com>
2023-04-09 22:46:24 +0530

    Create ProjectInfo.h

---
+++ ProjectInfo.h
@@ -0,0 +1,17 @@
+// namespace
+using namespace std;
+// header file
+#include <iostream>
+// Source
+
+// app name
+string app_Name = "Form maker";
+
+// version
+float app_Version = 0.1;
+
+// Machine
+string machine_Used = "MINGW GCC COMPILER, Cxx droid";
+
+// libraries used
+bool lib_used_in_app = false;
