85896131f5f3cd1186e46d0f0baa6c3e3aedc1a9
12356544567875 <109077342+12356544567875@users.noreply.github.com>
2023-04-10 00:17:56 +0530

    Create lib.h

---
+++ lib.h
@@ -0,0 +1,14 @@
+// This library includes al;l the header file important in this project
+
+// headerfiles
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include "var.h"
+
+
+// namespace
+using namespace std;
+
+
+// end();
